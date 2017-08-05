1554ff054fc92a95f14f6be1197f64295a7d936e
Roman Bienov <rodergstep@gmail.com>
2017-08-05 08:33:58 +0300

    final release

--- docs/mail.php
+++ docs/mail.php
@@ -1,19 +1,19 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Validate on empty fields
-        $to = 'will@croesusdigitalmarketing.com'; //Receiver
-        $subject = 'New Message from Croesus website';
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Validate on empty fields
+        $to = 'rodergstep@gmail.com'; //Receiver
+        $subject = 'Благоокна. Новая заявка';
         $message = '
                 <html>
                     <head>
                         <title>'.$subject.'</title>
                     </head>
                     <body>
-                        <p>Name: '.$_POST['name'].'</p>
-						            <p>Пошта: '.$_POST['email'].'</p>
-                        <p>Message: '.$_POST['formMessage'].'</p>
+                        <p>Тема: '.$_POST['remark'].'</p>
+                        <p>Имя: '.$_POST['name'].'</p>
+						<p>Телефон: '.$_POST['phone'].'</p>
                     </body>
                 </html>'; //We can use HTML tags
         $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-        $headers .= "From: Croesus <info@croesusdigitalmarketing.com>\r\n"; //Sender
+        $headers .= "From: Croesus <office@blagookna.ru>\r\n"; //Sender
         mail($to, $subject, $message, $headers);
 }
